a260151cdd2bbe377415bd44b55c4d573e506ee1
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-02 15:48:30 +0530

    garbage files removed

---
+++ MyfirstProject/MyfirstProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyfirstProject
+//
+//  Created by GDB Consultants on 02/11/15.
+//  Copyright (c) 2015 GDB Consultants. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
